eb0336a41e16ed465bbef9d1c177cc007f8a5a5a
LucaIenzi <Luca.ienzi@ocsbstudent.ca>
2017-12-21 17:20:48 -0500

    Update unit3-05-1.py

--- unit3-05-1.py
+++ unit3-05-1.py
@@ -1,3 +1,21 @@
+# Created by: luca.ienzi
+# Created on: nov 2017
+# Created for: ICS3U
+#  This program is a facorical program
+
+import ui
+
+
+def calculate(sender): 
+    user_answer = int(view['user_answer'].text)
+    if user_answer >= 0:
+      factorial = 1
+      while (user_answer >= 1):
+         factorial = factorial * user_answer
+         view['answer_lable'].text = "The answer is" + str (factorial)
+         user_answer = user_answer - 1
+view = ui.load_view()
+view.present('sheet')
 import ui
 
 
